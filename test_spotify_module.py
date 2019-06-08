c6e09a9d19ac1bd65e1c72386fccccbb4cdc23e0
Andy Gnias <AGnias47@gmail.com>
2019-06-08 15:38:58 -0400

    Standard test artifacts

--- test_spotify_module.py
+++ test_spotify_module.py
@@ -26,6 +26,8 @@ def get_artist_data_test(OAUTH_token, artist) :
 	SealID = get_artist_id(OAUTH_token, artist)
 	print("{}'s external URL: {}".format(artist, external_url))
 	print("{}'s ID: {}".format(artist, SealID))
+	print("{}'s external URL: {}".format(artist, external_url), file=open("Test_Artifacts/artist_data_test", "w"))
+	print("{}'s ID: {}".format(artist, SealID), file=open("Test_Artifacts/artist_data_test", "a"))
 	sleep(test_delay)
 
 def get_album_data_test(OAUTH_token, artist, SealID) :
@@ -33,6 +35,9 @@ def get_album_data_test(OAUTH_token, artist, SealID) :
 	print(get_album_data_by_artist(OAUTH_token, SealID))
 	print(get_album_data_by_artist(OAUTH_token, SealID, "href"))
 	print(get_album_data_by_artist(OAUTH_token, SealID, "id"))
+	print(get_album_data_by_artist(OAUTH_token, SealID), file=open("Test_Artifacts/album_data_test", "w"))
+	print(get_album_data_by_artist(OAUTH_token, SealID, "href"), file=open("Test_Artifacts/album_data_test", "a"))
+	print(get_album_data_by_artist(OAUTH_token, SealID, "id"), file=open("Test_Artifacts/album_data_test", "a"))
 	sleep(test_delay)
 
 def get_track_data_test(OAUTH_token, artist) :
@@ -40,6 +45,9 @@ def get_track_data_test(OAUTH_token, artist) :
 	print(get_tracks_by_album_id(OAUTH_token, "2Fd1KIL5aUNTl40H3OkOQi"))
 	print(get_tracks_by_album_id(OAUTH_token, "2Fd1KIL5aUNTl40H3OkOQi", "href"))
 	print(get_tracks_by_album_id(OAUTH_token, "2Fd1KIL5aUNTl40H3OkOQi", "id"))
+	print(get_tracks_by_album_id(OAUTH_token, "2Fd1KIL5aUNTl40H3OkOQi"), file=open("Test_Artifacts/track_data_test", "w"))
+	print(get_tracks_by_album_id(OAUTH_token, "2Fd1KIL5aUNTl40H3OkOQi", "href"), file=open("Test_Artifacts/track_data_test", "a"))
+	print(get_tracks_by_album_id(OAUTH_token, "2Fd1KIL5aUNTl40H3OkOQi", "id"), file=open("Test_Artifacts/track_data_test", "a"))
 
 if __name__ == "__main__" :
 	#Check if the OAuth token has been defined as an argument; if not, exit
