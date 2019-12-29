d1249bfbcd1e084255e7fee6847227e1917a469d
Andy Gnias <AGnias47@gmail.com>
2019-12-28 19:31:49 -0500

    Clean up generator

--- playlist_generator.py
+++ playlist_generator.py
@@ -61,32 +61,34 @@ def process_commandline_parameters():
 
 
 def main():
-    (OAuth_Token, Playlist_Filename, Playlist_Name, Description) = process_commandline_parameters()
+    (oauth_token, playlist_filename, playlist_name, description) = process_commandline_parameters()
 
     # Parse the tracks from the CSV
-    playlist_tracks = parse_playlist(Playlist_Filename)
+    playlist_tracks = parse_playlist(playlist_filename)
     if not playlist_tracks:
         sys.exit(f"{playlist_tracks} either does not exist or does not have any content; exiting")
 
     # Initialize the Playlist to be created
-    playlist = Playlist(Playlist_Name, playlist_tracks)
-    if not playlist.spotify_init(OAuth_Token, Description):
+    playlist = Playlist(playlist_name, playlist_tracks)
+    if not playlist.spotify_init(oauth_token, description):
         sys.exit("Playlist could not be created; exiting")
 
     # Add any tracks unable to be added to this list
     missed_tracks = list()
+    print(f"Adding songs from {playlist_filename} to {playlist.name}")
     for track in playlist_tracks:
-        print("Adding " + track.song + ": " + track.artist + " to " + playlist.name)
-        if track.spotify_query(OAuth_Token): # If track was found via search
-            if playlist.spotify_add_track(OAuth_Token, track.id): # add to playlist
-                print("Success")
-            else:
+        print(f"Adding {track.song} by {track.artist}")
+        if track.spotify_query(oauth_token):  # If track was found via search
+            if not playlist.spotify_add_track(oauth_token, track.id):  # add to playlist
                 missed_tracks.append(track.song + ", " + track.artist)
         else:
             missed_tracks.append(track.song + ", " + track.artist)
 
-    print("\nTracks unable to be found: ")
-    print(*missed_tracks, sep="\n")
+    if missed_tracks:
+        print("\nTracks unable to be found: ")
+        print(*missed_tracks, sep="\n")
+    else:
+        print("\nAll tracks added successfully\n")
 
 
 main()
