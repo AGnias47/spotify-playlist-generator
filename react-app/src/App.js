8e5aef3579406f30e6cd76b0cb1e8591f5db071e
Andy Gnias <AGnias47@gmail.com>
2020-10-22 22:03:35 -0400

    Starter react app

---
+++ react-app/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Spotify Playlist Generator</h1>
+      <h2>File Name</h2>
+      <h2>Playlist Name</h2>
+      <h2>Playlist Description</h2>
+    </div>
+  );
+}
+
+export default App;
