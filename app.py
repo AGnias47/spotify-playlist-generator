1d05d3583d1a3cfd8c90175aa5c3e55989d6877d
Andy Gnias <AGnias47@gmail.com>
2020-10-25 17:17:33 -0400

    Working login page

--- app.py
+++ app.py
@@ -11,9 +11,9 @@
 Adds a CSV of tracks to a Spotify playlist
 """
 
-import sys
-import argparse
-from json import JSONDecodeError
+import json
+from random import randint
+import urllib.parse
 
 from flask import Flask, render_template, Response, request, redirect, url_for
 
@@ -23,16 +23,33 @@ from src.Exceptions import PlaylistNotInitializedError
 from spotify_token_refresh.refresh import get_access_token
 
 
-
 app = Flask(__name__)
+
+with open("keys.json") as K:
+    keys = json.load(K)
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    client_id = keys["client_id"]
+    state = generate_random_string(16)
+    scope = "user-read-private user-read-email playlist-read-private playlist-modify-private playlist-modify-public"
+    redirect_uri = "http://localhost:5000/callback"
+    url = "https://accounts.spotify.com/authorize?"
+    url += f"response_type=code&client_id={client_id}"
+    url += f"&scope={scope}&redirect_uri={redirect_uri}&state={state}"
+    return redirect(url)
+
+
+@app.route("/callback")
+def callback():
+    return "A"
+
 
 @app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
-@app.route('/time')
-def get_current_time():
-    return {'time': time.time()}
 
 @app.route("/", methods=["POST"])
 def submit_form():
@@ -40,21 +57,35 @@ def submit_form():
     playlist_description = request.form["desc"]
     user_oauth_token = request.form["apikey"]
     playlist_content = request.form["playlist-content"]
-    return render_template('index.html')
-    
+    return render_template("index.html")
 
-@app.route("/submit", methods=['POST'])
+
+@app.route("/submit", methods=["POST"])
 def create_playlist():
     playlist_name = request.form["name"]
     playlist_description = request.form["desc"]
     user_oauth_token = request.form["apikey"]
     playlist_content = request.form["playlist-content"]
-    return render_template('submit.html')
+    return render_template("submit.html")
 
 
-def create_playlist(playlist_display_name, playlist_tracks, user_oauth_token, playlist_description):
+def generate_random_string(length):
+    text = str()
+    for i in range(0, length):
+        text += get_random_character()
+    return text
+
+
+def get_random_character():
+    random_ascii_chars = [[65, 90], [97, 122], [48, 57]]
+    rand_type = random_ascii_chars[randint(0, len(random_ascii_chars) - 1)]
+    return chr(randint(rand_type[0], rand_type[1]))
+
+
+def create_playlist_help(
+    playlist_display_name, playlist_tracks, user_oauth_token, playlist_description
+):
     playlist = Playlist(playlist_display_name, playlist_tracks)
     if not playlist.spotify_init(user_oauth_token, playlist_description):
         raise PlaylistNotInitializedError
     missed_tracks_list = playlist.spotify_add_tracks(user_oauth_token)
-
