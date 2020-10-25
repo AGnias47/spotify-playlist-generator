365dfdb6d4d54ae8dca9a5dcdecf701b6311ccc1
Andy Gnias <AGnias47@gmail.com>
2020-10-24 22:03:17 -0400

    Initial framework of app

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -14,11 +13,34 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-
-        ... no changes in this part ...
-
+        <h1>Spotify Playlist Generator</h1>
         <p>The current time is {currentTime}.</p>
       </header>
+      <form>
+        <label>
+          Playlist Name:
+          <input type="text" name="name" />
+        </label><br/>
+        <label>
+          Playlist Description:
+          <input type="text" name="desc" />
+        </label><br/>
+        <label>
+          API Key
+          <input type="text" name="apikey" />
+        </label><br/>
+        <label>Artist Song Separator   </label>
+        <select>
+          <option value="comma">,</option>
+          <option selected value="dash">-</option>
+          <option value="slash">/</option>
+        </select><br/>
+        <label>
+          Playlist<br/>
+          <textarea/>
+        </label><br/>
+        <input type="submit" value="Submit" />
+      </form>
     </div>
   );
 }
